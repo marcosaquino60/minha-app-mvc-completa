8725210cda77ecaaa55f3249601fcac2eca6d20e
Marcos Aquino <marcoskim60@gmail.com>
2024-02-29 18:28:54 -0300

    Criação App Mvc Completa até o acesso ao Banco por Repository

---
+++ DevIO.App/Startup.cs
@@ -0,0 +1,56 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Data.Context;
+using DevIO.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.App
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            services.AddDbContext<MeuDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddScoped<MeuDbContext> ();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+        }
+    }
+}
---
+++ DevIO.Data/Mapping/EnderecoMapping.cs
@@ -0,0 +1,45 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Mapping
+{
+    public class EnderecoMapping : IEntityTypeConfiguration<Endereco>
+    {
+        public void Configure(EntityTypeBuilder<Endereco> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Logradouro)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(e => e.Numero)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.Property(e => e.Cep)
+                .IsRequired()
+                .HasColumnType("varchar(8)");
+
+            builder.Property(e => e.Bairro)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+            
+            builder.Property(e => e.Cidade)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+            
+            builder.Property(e => e.Cidade)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.ToTable("Enderecos");
+        }
+    }
+}
